9b287cd8253b2980f80247195bdaea157140dd09
Steve Martocci <smartocci@gmail.com>
2008-01-08 17:32:10 +0000

    facebook auth
    git-svn-id: https://sympact-adiserver.svn.ey02.engineyard.com/src/trunk/plugins/younety_client@414 753f6b9c-c39a-4ea0-9e70-2ccbdc17162d

--- lib/authenticators/facebook_authenticator.rb
+++ lib/authenticators/facebook_authenticator.rb
@@ -8,8 +8,8 @@ module Younety
         end
   
         def facebook_authentication
-          require_facebook_install
-          #redirect_to fbsession.get_install_url
+          require_facebook_login
+          
           #return false
           return fbsession.is_valid? ? finish_facebook_login : false
         end
@@ -17,6 +17,9 @@ module Younety
         def finish_facebook_login
           facebook_user = FacebookYouser.find_or_create_by_facebook_session(fbsession)
           self.current_account = Account.find(facebook_user.account_id)
+          unless self.current_account.facebook.appAdded?
+            redirect_to fbsession.get_install_url and return
+          end
           logged_in? ? successful_login : failed_login( "Failed Authentication")
         end
     
