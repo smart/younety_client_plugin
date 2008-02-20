869cdd65dd52c8e0d39f110ddd5f5c255ef1e64e
Steve Martocci <smartocci@gmail.com>
2008-02-20 15:30:51 +0000

    created icon system for webapps and shares
    git-svn-id: https://sympact-adiserver.svn.ey02.engineyard.com/src/trunk/plugins/younety_client@488 753f6b9c-c39a-4ea0-9e70-2ccbdc17162d

--- lib/remote/lib/share.rb
+++ lib/remote/lib/share.rb
@@ -6,6 +6,16 @@ module Younety
       def self.share_it(share_id, adi_id, params = {})
         Share.new(:id => share_id).post(:go, { :adi_id => adi_id, :share_it => params})
       end
+      
+      def icon_path
+        icon_name = self.name.downcase.gsub(' ', '_').gsub(".","").gsub("\'", "").gsub("&", "and").gsub("_webapp", "").gsub("_share", "")
+        if defined?(YOUNETY_LOCAL_ICON_PATH)
+          #do something
+        else
+          "#{YOUNETY['url']}/images/icons/shares/#{icon_name}.png"
+        end
+      end
+      
     end
   end
 end
--- lib/remote/lib/webapp.rb
+++ lib/remote/lib/webapp.rb
@@ -5,6 +5,15 @@ module Younety
       def self.shares(webapp_id)
         Webapp.new(:id => webapp_id).get(:shares)
       end
+      
+      def icon_path
+        icon_name = self.name.downcase.gsub(' ', '_').gsub(".","").gsub("\'", "").gsub("&", "and").gsub("_webapp", "").gsub("_share", "")
+        if defined?(YOUNETY_LOCAL_ICON_PATH)
+          #do something
+        else
+          "#{YOUNETY['url']}/images/icons/webapps/#{icon_name}.png"
+        end
+      end
     end
   end
 end
